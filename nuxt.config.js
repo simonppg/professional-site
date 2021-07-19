df03d549d422f3a2a6809b89f4c7d90986fea997
simonppg <simonppg@gmail.com>
2021-07-19 09:46:18 -0700

    Install tailwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,9 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
+    '@nuxt/typescript-build',
+    // https://tailwindcss.com/docs/guides/nuxtjs
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules

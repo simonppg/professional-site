d2db17a0cc5b4d8754b0fcb0204a268ecf9aef54
simonppg <simonppg@gmail.com>
2022-04-11 20:09:16 -0700

    style(audioplugin): fix Property does not exist on type CombinedVueInstance

--- plugins/AudioPlugin.ts
+++ plugins/AudioPlugin.ts
@@ -1,6 +1,27 @@
 import { Plugin } from '@nuxt/types'
 import AudioApp from '../src/AudioApp'
 
+declare module 'vue/types/vue' {
+  interface Vue {
+    $audioApp: AudioApp
+  }
+}
+declare module '@nuxt/types' {
+  interface NuxtAppOptions {
+    $audioApp: AudioApp
+  }
+  interface Context {
+    $audioApp: AudioApp
+  }
+}
+
+declare module 'vuex/types/index' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+  interface Store<S> {
+    $audioApp: AudioApp
+  }
+}
+
 const myPlugin: Plugin = (_, inject) => {
   inject('audioApp', new AudioApp())
 }

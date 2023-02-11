46e0145bef10ddba2f5d7ddc3275d55321cfd3b0
simonppg <simonppg@gmail.com>
2023-02-11 16:51:54 -0700

    feat(audio): example of Attack Decay Sustain and Release

--- src/AudioApp.ts
+++ src/AudioApp.ts
@@ -8,6 +8,81 @@ export default class AudioApp {
   private oscilatorNode: OscillatorNode | undefined
   private oscilatorTypes: OscillatorType[] = ['sine', 'square', 'triangle', 'sawtooth']
   private pitches = [830, 174]
+  private STAGE_MAX_TIME = 1
+  private ADSR = { attack: 0.9, decay: 10, sustain: 10, release: 0.9 }
+  // private ADSR = { attack: 0.2, decay: 0, sustain: 1, release: 0.3 }
+
+  playExample () {
+    this.context = new AudioContext()
+    if (!this.context) { throw new Error('Not supported :(') }
+    this.gainNode = this.context.createGain()
+    this.gainNode.connect(this.context.destination)
+
+    // this.basicExample()
+    // this.thickerTone()
+    this.onOffNote()
+  }
+
+  private onOffNote () {
+    this.noteOn(440)
+    this.noteOff()
+  }
+
+  private thickerTone () {
+    const oscList = new Array(3)
+    const frequency = 440
+    const unisonWidth = 10
+    oscList[0] = this.newOscillator(frequency, 0)
+    oscList[1] = this.newOscillator(frequency, -unisonWidth)
+    oscList[2] = this.newOscillator(frequency, unisonWidth)
+
+    oscList[0].stop(this.context.currentTime + 2)
+    oscList[1].stop(this.context.currentTime + 2)
+    oscList[2].stop(this.context.currentTime + 2)
+  }
+
+  private basicExample () {
+    const osc = this.context.createOscillator()
+    osc.type = 'sawtooth'
+    osc.frequency.value = 440
+    osc.connect(this.context.destination)
+    osc.start()
+    osc.stop(this.context.currentTime + 2)
+  }
+
+  private newOscillator (frequency : number, detune: number): OscillatorNode {
+    const osc = this.context.createOscillator()
+    osc.type = 'sawtooth'
+    osc.frequency.value = frequency
+    osc.detune.value = detune
+    osc.start()
+    return osc
+  }
+
+  private noteOn (frequency: number) {
+    this.gainNode?.gain.cancelScheduledValues(this.context?.currentTime)
+    const osc = this.newOscillator(frequency, 0)
+    osc.connect(this.gainNode)
+
+    const now = this.context.currentTime
+    const attackDuration = this.ADSR.attack * this.STAGE_MAX_TIME
+    const attackEndTime = now + attackDuration
+    const decayDuration = this.ADSR.decay * this.STAGE_MAX_TIME
+
+    this.gainNode?.gain.setValueAtTime(0, this.context?.currentTime)
+    this.gainNode?.gain.linearRampToValueAtTime(1, attackEndTime)
+    this.gainNode?.gain.setTargetAtTime(this.ADSR.sustain, attackEndTime, decayDuration)
+  }
+
+  private noteOff () {
+    this.gainNode?.gain.cancelScheduledValues(this.context?.currentTime)
+
+    const now = this.context?.currentTime
+    const releaseDuration = this.ADSR.release * this.STAGE_MAX_TIME
+    const releaseEndTime = now + releaseDuration
+    this.gainNode?.gain.setValueAtTime(this.gainNode.gain.value, now)
+    this.gainNode?.gain.linearRampToValueAtTime(0, releaseEndTime)
+  }
 
   play () {
     this.context = new AudioContext()

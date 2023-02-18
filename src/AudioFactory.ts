f26ed3b0ebf7e4515ae51863db86b6bd5261127f
simonppg <simonppg@gmail.com>
2023-02-18 00:09:38 -0700

    Organize examples

--- src/AudioFactory.ts
+++ src/AudioFactory.ts
@@ -1,5 +1,8 @@
+type OscillatorType = 'custom' | 'sawtooth' | 'sine' | 'square' | 'triangle'
+
 export default class AudioFactory {
   private audioContext: AudioContext
+  private oscilatorTypes: OscillatorType[] = ['sine', 'square', 'triangle', 'sawtooth']
 
   constructor (audioContext: AudioContext) {
     this.audioContext = audioContext
@@ -16,5 +19,16 @@ export default class AudioFactory {
     osc.frequency.value = frequency
     osc.detune.value = detune
     return osc
+  }
+
+  public randomOscilator () {
+    const oscilator = this.newOscillator(440, 0)
+    const randomIndex = this.randomIndex(this.oscilatorTypes)
+    oscilator.type = this.oscilatorTypes[randomIndex]
+    return oscilator
+  }
+
+  private randomIndex (arr: any[]) {
+    return Math.floor(Math.random() * arr.length)
   }
 }
